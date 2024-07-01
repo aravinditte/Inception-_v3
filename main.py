ed7b58e63690ae8fe5e76e69948d7bc1a1d722e3
Aravind Itte <119928913+aravinditte@users.noreply.github.com>
2024-07-01 23:31:48 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from google.colab import files
-
-uploaded = files.upload()
-
-for fn in uploaded.keys():
-  print('User uploaded file "{name}" with length {length} bytes'.format(
-      name=fn, length=len(uploaded[fn])))
-
-!mkdir -p ~/.kaggle/ && mv kaggle.json ~/.kaggle/ && chmod 600 ~/.kaggle/kaggle.json
